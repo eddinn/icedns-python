6ec0b19acab2a507da74eea25255f74f670cd2d9
Edvin Dunaway <edvin@lsh.is>
2019-10-21 14:45:10 +0000

    update comments and bump version

--- icedns.py
+++ icedns.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 import sys, ipaddress, socket
 
-# Don't do anything if we are missing the arg or args are more than one
+"""
+Author:   Edvin Dunaway
+Email:    edvin@eddinn.net
+Version:  0.1.2
+Licence:  MIT
+"""
+
+# Don't do anything, return Usage and exit if we are missing
+# the arg or args are more than one.
 if len(sys.argv) != 2:
   print('Usage: icedns.py <ipaddress>')
   sys.exit(1)
@@ -10,7 +18,7 @@ if len(sys.argv) != 2:
 ip = sys.argv[1]
 # Reverse the IP and append the arpa
 revip = '.'.join(reversed(ip.split('.'))) + '.iceland.rix.is'
-# Try for the local resolve and return True
+# Try for the local resolve and return true if True
 try:
   if socket.gethostbyname(revip) == '127.1.0.1':
     print(ip, 'is an .is IP')
