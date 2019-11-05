9f7d4fb922fdf10526de83ced73b76fe937e847f
Edvin Dunaway <edvin@lsh.is>
2019-11-05 14:00:16 +0000

    addinv .circleci config.yml, made the lookup in a function

--- icedns.py
+++ icedns.py
@@ -10,17 +10,24 @@ import sys, socket
 # the arg or args are more than one.
 if len(sys.argv) != 2:
   print('Usage: icedns.py <ipaddress>')
-  sys.exit(1)
+  exit(1)
 
 # Get the IP from argv
 ip = sys.argv[1]
-# Reverse the IP and append the arpa
-revip = '.'.join(reversed(ip.split('.'))) + '.iceland.rix.is'
-# Try for the local resolve and return true if True
-try:
-  if socket.gethostbyname(revip) == '127.1.0.1':
-    print(ip, 'is an .is IP')
-# Else return False
-except Exception as e:
-  print(ip, 'is not an .is IP -', format(e))
-  exit(1)
+
+# Define the funtion to look up the reversed IP
+def lookup():
+  # Reverse the IP and append the arpa host
+  revip = '.'.join(reversed(ip.split('.'))) + '.iceland.rix.is'
+  # Try for the local resolve and return true if True
+  try:
+    if socket.gethostbyname(revip) == '127.1.0.1':
+      print(ip, 'is an .is IP')
+      exit(0)
+  # Else return False
+  except Exception as e:
+    print(ip, 'is not an .is IP -', format(e))
+    exit(1)
+
+# Run the function with given argv
+lookup()
