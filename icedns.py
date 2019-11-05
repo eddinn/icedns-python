48bfc86e1bf801a3fc5937e90c17a43686d01a34
Edvin Dunaway <edvin@lsh.is>
2019-11-05 14:17:25 +0000

    flake8 linting

--- icedns.py
+++ icedns.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-import sys, socket
+import socket
+import sys
 
 # Author:   Edvin Dunaway
 # Email:    edvin@eddinn.net
@@ -9,25 +10,27 @@ import sys, socket
 # Don't do anything, return Usage and exit if we are missing
 # the arg or args are more than one.
 if len(sys.argv) != 2:
-  print('Usage: icedns.py <ipaddress>')
-  exit(1)
+    print('Usage: icedns.py <ipaddress>')
+    exit(1)
 
 # Get the IP from argv
 ip = sys.argv[1]
 
+
 # Define the funtion to look up the reversed IP
 def lookup():
-  # Reverse the IP and append the arpa host
-  revip = '.'.join(reversed(ip.split('.'))) + '.iceland.rix.is'
-  # Try for the local resolve and return true if True
-  try:
-    if socket.gethostbyname(revip) == '127.1.0.1':
-      print(ip, 'is an .is IP')
-      exit(0)
-  # Else return False
-  except Exception as e:
-    print(ip, 'is not an .is IP -', format(e))
-    exit(1)
+    # Reverse the IP and append the arpa host
+    revip = '.'.join(reversed(ip.split('.'))) + '.iceland.rix.is'
+    # Try for the local resolve and return true if True
+    try:
+        if socket.gethostbyname(revip) == '127.1.0.1':
+            print(ip, 'is an .is IP')
+            exit(0)
+    # Else return False
+    except Exception as e:
+        print(ip, 'is not an .is IP -', format(e))
+        exit(1)
+
 
 # Run the function with given argv
 lookup()
